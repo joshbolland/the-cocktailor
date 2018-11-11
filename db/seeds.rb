a171d3f67d408e044f200e4aa01ed05e80b4afe2
Josh Bolland <jjm.bolland@gmail.com>
2018-11-11 20:38:49 +0000

    changed seed file added destroy cocktails

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'open-uri'
 Ingredient.destroy_all
+Cocktail.destroy_all
 
 # populate ingredients
 url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
